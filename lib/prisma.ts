d4924f38e55f7b8e4da1271f3dcd9c44144e5e49
phileasmah <60140749+phileasmah@users.noreply.github.com>
2021-06-04 22:56:34 +0800

    prisma fix

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient({
-  log: ["info"]
-});
+interface CustomNodeJsGlobal extends NodeJS.Global {
+  prisma: PrismaClient;
+}
+
+declare const global: CustomNodeJsGlobal;
+
+const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV === "development") global.prisma = prisma;
+
+
 export default prisma;
