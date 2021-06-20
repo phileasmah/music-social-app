09832e22858be2e0538db40f8fe00b91a3358fea
phileasmah <60140749+phileasmah@users.noreply.github.com>
2021-06-20 16:17:32 +0800

    change darkgrey3 colour

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,7 +32,7 @@ module.exports = {
         lightgrey3: "#373737",
         darkgrey: "#202124",
         darkgrey2: "#232323",
-        darkgrey3: "#2E2B2E"
+        darkgrey3: "#313235"
       },
       boxShadow: {
         custom: "0 4px 5px 0 rgb(0 0 0 / 14%), 0 1px 10px 0 rgb(0 0 0 / 12%), 0 2px 4px -1px rgb(0 0 0 / 20%)"
