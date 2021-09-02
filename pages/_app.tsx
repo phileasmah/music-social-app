21fa50ff985352ca54a19c6534026eddfd3adbd9
phileasmah <60140749+phileasmah@users.noreply.github.com>
2021-09-02 11:21:32 +0800

    updates

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { Provider } from "next-auth/client";
-import { AppProps } from "next/dist/next-server/lib/router/router";
+import { AppProps } from "next/app";
 import Router, { useRouter } from "next/router";
 import NProgress from "nprogress";
 import "nprogress/nprogress.css";
